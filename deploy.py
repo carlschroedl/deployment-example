e4925259971c18722a947089a8ce39305e75f3dd
Carl Schroedl <carlschroedl@gmail.com>
2020-11-22 20:21:20 -0600

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,11 +3,11 @@ import boto3
 import random
 import os
 
-bucket_id = random.randint(0, 10000000000)
+stack_id = random.randint(0, 10000000000)
 
 client = boto3.client('cloudformation')
 
-template = {'Resources': {'S3Bucket': {'Type': 'AWS::S3::Bucket', 'DeletionPolicy': 'Delete', 'Properties': {'BucketName': 'throwaway-elasti-cd-{}'.format(bucket_id)}}}}
+template = {'Resources': {'S3Bucket': {'Type': 'AWS::S3::Bucket', 'DeletionPolicy': 'Delete', 'Properties': {'BucketName': 'throwaway-elasti-cd-{}'.format(stack_id)}}}}
 template_str = json.dumps(template)
 region = os.environ.get('AWS_REGION')
 role_arn = 'arn:aws:iam::{}:role/cf-deployment-service'.format(region)
